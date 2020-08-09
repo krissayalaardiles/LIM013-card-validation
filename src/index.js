aa08f19bf6045a8e3327926bb9417aa64389d38d
kriss Ayala <krisspaolafayala@gmail.com>
2020-08-08 20:45:35 -0500

    commit final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
-console.log(validator);
+let num = document.getElementById('getData');
+
+num.addEventListener("click", function(){
+  let numero = document.getElementById('Numero').value;
+  const resultado = validator.isValid(numero);
+  let numTarjeta = validator.maskify(numero);
+  
+
+  let traerdiv = document.getElementById("mostrar");
+    if (resultado === true) {
+      const anuncio = "¡Gracias por su compra!";
+      traerdiv.innerHTML = anuncio;
+    }else{
+      traerdiv.innerHTML = "Tarjeta Invalida";
+  }
+
+  let divmichi = document.getElementById("enmascarado");
+    divmichi.innerHTML = numTarjeta;
+
+
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
+
 const validator = {
-  // ...
-};
+  isValid:
+  function(numeroDeTarjeta){
+
+         //arreglo eslavones
+         let arregloTarjeta = numeroDeTarjeta.split('');
+
+         //reverso de tarjeta
+         arregloTarjeta = arregloTarjeta.reverse();
+
+
+    let suma = 0;
+    for (let i = 0; i < arregloTarjeta.length; i++) {
+
+      let parposition = parseInt(arregloTarjeta[i]);
+      if ((i % 2) != 0) {
+       parposition *= 2;
+       if (parposition > 9) {
+           parposition -= 9;
+           }
+          }
+       suma += parposition;
+     }
+     
+     if(suma!==0){
+        if ((suma %10)==0){
+           return true;
+        }else{
+          return false;
+        }
+      }else{
+        return false;
+      }
+ },
+ maskify: function(numeroDeTarjeta){
+    let michi = numeroDeTarjeta.replace(/.(?=.{4,}$)/g, '#');
+return michi;
+ }
+}
 
 export default validator;
